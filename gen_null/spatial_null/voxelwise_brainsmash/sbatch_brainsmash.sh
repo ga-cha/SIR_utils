a1ed525f360bc3d60075a49dc4dde74cfcdeac8d
Gabriella Chan (Monash University) <gchan@m3t104.massive.org.au>
2025-02-16 12:51:29 +1100

    Completed body of voxelwise brainsmash. Pickles and unpickles generator for speed and checks uniqueness of output null maps.

--- gen_null/spatial_null/voxelwise_brainsmash/sbatch_brainsmash.sh
+++ gen_null/spatial_null/voxelwise_brainsmash/sbatch_brainsmash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-out=slurm_vox_bs.log
+out=sbatch_out.log
 py_sir=/fs03/kg98/gchan/conda_envs/sir/bin/python
 
 for i in {1..999}
@@ -8,8 +8,8 @@ do
     sbatch --job-name="$i brainsmash" --output="$out" <<EOL
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=12
-#SBATCH --time=24:00:00
+#SBATCH --ntasks-per-node=13
+#SBATCH --time=36:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gabriella.chan@monash.edu
 
